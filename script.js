457b4e7fba3b0e37fb40f1e2ff526f59d5334ad1
Daniel pereira <daniel.dev@hotmail.com.br>
2022-03-12 10:24:37 -0300

    reset all code.

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
-var tentativas = 4;
-var pontos = 0;
-function chute(){
+// 
+var NumSec = Math.floor(Math.random() * 11);
+var attempts = 2;
+
+    function chute(){
             
-    var NumSec = Math.floor(Math.random() * 6);
-    var txtn1 = document.getElementById('txtnum');
-    var res = document.getElementById('answer');
-    var points = document.getElementById('points');
-    var num = Number(txtn1.value);
+    var num = document.getElementById('txtnum').value;
+    var answer = document.getElementById('answer');
+    // var points = document.getElementById('points');
     
     // exibir pontos na tela
-     points.innerHTML = `${pontos}` 
+    // points.innerHTML = `${pontos}` 
      
         if(num == ""){
     
-            res.innerHTML = `Digite um número`
+            answer.innerHTML = `Digite um número`
         }
         else if(num == NumSec){
 
-            res.innerHTML = `Você acertou!`
-            
-            // add points and reset attempts
-            if(num == NumSec){
-
-                pontos += 1;
-            }
-            tentativas = 2;
+            answer.innerHTML = `Você acertou!`
         }
         else if(num > 10 || num < 0){
     
-            res.innerHTML = `Digite um número de 0 e 10.`
+            answer.innerHTML = `Digite um número de 0 e 10.`
         }
-        else if(num != NumSec && tentativas > 0)
+        else if(num != NumSec && attempts > 0)
         {
-            res.innerHTML = `Você errou! <br> <span>Tentativas restantes:<span> ${tentativas}`
-
-            // remove points and attempts
-            if(pontos > 0){
-                pontos -= 1;
-            }
-            tentativas -= 1;
+            answer.innerHTML = `Você errou! <br> <span>Tentativas restantes:<span> ${attempts}`
+            attempts -= 1;
            
         }
         else{
-            res.innerHTML = 'Você perdeu!<br><br> <button> <a href="../index.html">Jogar novamente</a></button>'
+            answer.innerHTML = 'Você perdeu!<br><br> <button> <a href="../index.html">Jogar novamente</a></button>'
         }
     }
